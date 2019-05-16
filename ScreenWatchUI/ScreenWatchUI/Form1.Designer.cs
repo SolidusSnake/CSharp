447152988f4ef0b0e2d71a96f4a7253a44444111
Wesley Tomlinson <SolidusSnake@users.noreply.github.com>
2019-05-15 23:24:18 -0500

    Create ScreenWatchUI_WinForm

---
+++ ScreenWatchUI/ScreenWatchUI/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ScreenWatchUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelRuntime = new System.Windows.Forms.Label();
+            this.labelElapsed = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelRuntime
+            // 
+            this.labelRuntime.AutoSize = true;
+            this.labelRuntime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRuntime.ForeColor = System.Drawing.Color.Lime;
+            this.labelRuntime.Location = new System.Drawing.Point(12, 9);
+            this.labelRuntime.Name = "labelRuntime";
+            this.labelRuntime.Size = new System.Drawing.Size(122, 31);
+            this.labelRuntime.TabIndex = 0;
+            this.labelRuntime.Text = "Runtime";
+            this.labelRuntime.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelElapsed
+            // 
+            this.labelElapsed.AutoSize = true;
+            this.labelElapsed.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelElapsed.Location = new System.Drawing.Point(12, 65);
+            this.labelElapsed.Name = "labelElapsed";
+            this.labelElapsed.Size = new System.Drawing.Size(0, 31);
+            this.labelElapsed.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(297, 105);
+            this.Controls.Add(this.labelElapsed);
+            this.Controls.Add(this.labelRuntime);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ScreenWatch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelRuntime;
+        private System.Windows.Forms.Label labelElapsed;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ScreenWatchUI/ScreenWatchUI/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenWatchUI
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        Stopwatch watch;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = (5000);
+            timer.Tick += new EventHandler(timer_Tick);
+            watch = new Stopwatch();
+            timer.Start();
+            watch.Start();
+
+
+            //System.Threading.Thread.Sleep(5000); // time in ms
+            //
+
+            //labelElapsed.Text = elapsedTime;
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = watch.Elapsed;
+            string elapsedTime = String.Format("{0:00}h {1:00}m {2:00}s", ts.Hours, ts.Minutes, ts.Seconds);
+
+            labelElapsed.Text = elapsedTime;
+
+            //labelElapsed.Text = watch.Elapsed.Hours.ToString() + watch.Elapsed.Minutes.ToString() + watch.Elapsed.Seconds.ToString();
+            SendKeys.SendWait("{NUMLOCK}");
+
+            Application.DoEvents();
+        }
+    }
+}
